b99383e7ae9853f6c375e97d7a054c426d3e77ef
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-13 20:15:57 -0600

    Add manufacturer and guitar entries to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Manufacturers
+fender = Manufacturer.create(brand: "Fender", days_since_last_incident: 109, domestic: false)
+gibson = Manufacturer.create(brand: "Gibson", days_since_last_incident: 54, domestic: false)
+prs = Manufacturer.create(brand: "PRS", days_since_last_incident: 259, domestic: true)
+taylor = Manufacturer.create(brand: "Taylor", days_since_last_incident: 369, domestic: false)
+
+# Guitars
+gibson.guitars.create!(model: "Explorer", price: 899.99, sold: false)
+gibson.guitars.create(model: "Les Paul", price: 2999.99, sold: true)
+fender.guitars.create(model: "Telecaster", price: 1199.99, sold: true)
+fender.guitars.create(model: "Stratocaster", price: 1499.99, sold: false)
+prs.guitars.create(model: "CE Standard 24", price: 899.99,sold: false)
+prs.guitars.create(model: "Silver Sky", price: 1999.99, sold: false)
+taylor.guitars.create(model: "C214 Grand Parlor", price: 899.99, sold: false)
