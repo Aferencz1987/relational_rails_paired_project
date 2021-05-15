08f3fde7b1940a0b0ed12156d43a969a6397d0cd
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-15 14:42:20 -0600

    Writes test and implements guitar information

--- spec/features/guitars/show_spec.rb
+++ spec/features/guitars/show_spec.rb
@@ -1,10 +1,26 @@
-#[ ] done
+require 'rails_helper'
 
-#User Story 4, Child Show (x2)
+RSpec.describe 'Guitar information' do
 
-#As a visitor
-#When I visit '/child_table_name/:id'
-#Then I see the child with that id including the child's attributes:
+  before(:all) do
+    manufacturer = Manufacturer.create!(brand: "Simon's Importers", days_since_last_incident: 0, domestic: true)
+    @sg = manufacturer.guitars.create!(model: "SG", price: 1799.99, sold: true)
+    @id = @sg.id.to_s
+  end
+
+  it 'shows detailed information guitar' do
+    #[ ] done
+
+    #User Story 4, Child Show (x2)
+
+    #As a visitor
+    #When I visit '/child_table_name/:id'
+    #Then I see the child with that id including the child's attributes:
+    visit '/guitars/' + @id
+    expect(page).to have_content("Out of stock")
+    expect(page).to have_content("$1799.99")
+    expect(page).to have_content("SG")
+  end
 
 #[ ] done
 
@@ -31,3 +47,4 @@
 #Then a 'DELETE' request is sent to '/child_table_name/:id',
 #the child is deleted,
 #and I am redirected to the child index page where I no longer see this child
+end
